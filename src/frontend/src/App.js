5da420aa37d20039442bde448e5f2255f36f6e05
Abhishek9934 <abhiigupta9934@gmail.com>
2021-03-24 23:16:29 +0530

    Home page started

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LogIn from './main/login';
-
+import SignUp from './main/sign-up';
+import Dashboard from './main/home';
 function App() {
   return (
     <div className="App">
-      <LogIn />
-
+      {/* <LogIn />
+      <SignUp /> */}
+      <Dashboard />
     </div>
   );
 }
