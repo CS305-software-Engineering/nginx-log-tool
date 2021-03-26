cb5893c32049322aa88ea4da41a39efb50821cc2
grtcoder <divyanshumathpal@gmail.com>
2021-03-27 00:57:40 +0530

    Added multithreaded implementation of collection agent and, local storage of data.

--- src/collector/collectionAgent/system/systemCollectionAgent.py
+++ src/collector/collectionAgent/system/systemCollectionAgent.py
@@ -1,11 +1,30 @@
 import psutil
-
+import sys
+sys.path.append('LogTool/')
+from utility.threads import threaded
 class systemCollectionAgent():
     def __init__(self):
         self.data={}
-    def cpuUsage():
-        x=psutil.Process()
-        return (x.cpu_percent())       
+        self.collectorFunctions=[ 
+            self.cpuUsage,
+            self.cpuCount
+        ]
+    @threaded
+    def cpuUsage(self):
+        self.data['cpuPercent']=psutil.cpu_percent(1)
+        print('a')
+    @threaded
+    def cpuCount(self):
+        self.data['cpuCount']=psutil.cpu_count()
+        print('b')
     
-a=systemCollectionAgent
-print(a.cpuUsage())
+    def setData(self):
+        handles=[]
+        for i in self.collectorFunctions:
+            handles.append(i())
+        for thread in handles:
+            thread.join()
+        print('threads completed')
+
+
+# print(a.cpuCount())
